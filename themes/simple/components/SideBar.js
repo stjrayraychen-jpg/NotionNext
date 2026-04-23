51d952b1848957f31ca4506ffc10a828e3e0d96a
stjrayraychen-jpg <stjrayraychen@gmail.com>
2026-04-23 18:22:25 +0800

    Update SideBar.js

--- themes/simple/components/SideBar.js
+++ themes/simple/components/SideBar.js
@@ -1,26 +1,16 @@
 import { AdSlot } from '@/components/GoogleAdsense'
-import Live2D from '@/components/Live2D'
+import OrangeCat from './OrangeCat'
 import Announcement from './Announcement'
 import Catalog from './Catalog'
 import WWAds from '@/components/WWAds'
 
-/**
- * 侧边栏
- * @param {*} props
- * @returns
- */
 export default function SideBar (props) {
   const { notice } = props
   return (<>
-
             <Catalog {...props} />
-
-            <Live2D />
-
+            <OrangeCat />
             <Announcement post={notice} />
-
             <AdSlot/>
             <WWAds orientation="vertical" className="w-full" />
-
     </>)
 }
