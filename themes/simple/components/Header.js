f647fa5750f99b5888c9088027915d072e2cf68e
stjrayraychen-jpg <stjrayraychen@gmail.com>
2026-04-23 17:40:50 +0800

    Update Header.js

--- themes/simple/components/Header.js
+++ themes/simple/components/Header.js
@@ -9,16 +9,35 @@ export default function Header(props) {
   return (
     <header className='text-center items-center px-6 relative z-10' style={{backgroundColor: '#FDF6EC'}}>
       <div className='flex flex-col items-center justify-center py-8'>
-
-        {/* 顶部大标题 喵Guide */}
         <div className='text-4xl font-serif font-bold tracking-widest mb-6' style={{color: '#8B4513'}}>
           喵Guide
         </div>
-
-        {/* 头像+名字+简介 */}
         <SmartLink href='/'>
           <div className='flex space-x-6 justify-center items-center'>
-            <div className='hover:rotate-45 hover:scale-1
-</header>
-  )
-}
+            <div className='hover:rotate-45 hover:scale-125 transform duration-200 cursor-pointer'>
+              <LazyImage
+                priority={true}
+                src={siteInfo?.icon}
+                className='rounded-full'
+                width={100}
+                height={100}
+                alt={siteConfig('AUTHOR')}
+              />
+            </div>
+            <div className='flex-col flex justify-center'>
+              <div className='text-2xl font-serif dark:text-white py-2 hover:scale-105 transform duration-200'>
+                {siteConfig('AUTHOR')}
+              </div>
+              <div
+                className='font-light dark:text-white py-2 hover:scale-105 transform duration-200 text-center'
+                dangerouslySetInnerHTML={{
+                  __html: siteConfig('SIMPLE_LOGO_DESCRIPTION', null, CONFIG)
+                }}
+              />
+            </div>
+          </div>
+        </SmartLink>
+        <div className='flex justify-center mt-2'>
+          <SocialButton />
+        </div>
+        <div className='text-xs mt-4 tracking-wide
