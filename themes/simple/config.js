03e14c1a9e68092e2640819562a41571a84e723d
stjrayraychen-jpg <stjrayraychen@gmail.com>
2026-08-05 19:29:09 +0800

    Update config.js

--- themes/simple/config.js
+++ themes/simple/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   SIMPLE_LOGO_IMG: '/Logo.webp',
   SIMPLE_TOP_BAR: true, // 显示顶栏
   SIMPLE_TOP_BAR_CONTENT: process.env.NEXT_PUBLIC_THEME_SIMPLE_TOP_TIPS || '',
-  SIMPLE_LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>美卡教程<br/>/图文讲解<br/>/美卡手把手保姆教程</div>',
+  SIMPLE_LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || 'miaoguide|喵guide|逻辑清晰的美卡薅羊毛教程',
 
   SIMPLE_AUTHOR_LINK: process.env.NEXT_PUBLIC_AUTHOR_LINK || '#',
 
